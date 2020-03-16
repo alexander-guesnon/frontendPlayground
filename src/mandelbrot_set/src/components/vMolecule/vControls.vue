227af65a34892246479320c04603356a2a60e0de
alexander.guesnon <alexander.guesnon@gmail.com>
2020-03-15 20:45:37 -0700

    control placement

--- src/mandelbrot_set/src/components/vMolecule/vControls.vue
+++ src/mandelbrot_set/src/components/vMolecule/vControls.vue
@@ -18,10 +18,25 @@ export default class VControls extends Vue {}
 </script>
 
 <style lang="sass">
+$min-top: 125px
+$min-left: 75px
 .vControls
   position:relative
   top: 0
   width: 300px
   height: 300px
   background-color: grey
+
+  .vButton:nth-child(1)
+    top: $min-top
+    left: $min-left
+  .vButton:nth-child(2)
+    top: $min-top
+    left: $min-left + 100px
+  .vButton:nth-child(3)
+    top: $min-top + 100px
+    left: $min-left
+  .vButton:nth-child(4)
+    top: $min-top + 100px
+    left: $min-left + 100px
 </style>
